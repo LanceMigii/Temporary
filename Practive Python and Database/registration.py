d463659f9f082bdd68e314b176c37723615b100f
LanceMigii <133740971+LanceMigii@users.noreply.github.com>
2023-05-22 09:41:36 +0800

    Update registration.py

--- Practive Python and Database/registration.py
+++ Practive Python and Database/registration.py
@@ -32,7 +32,35 @@ def register_user(username, password):
             cursor.close()
             connection.close()
 
+def login_user(username, password):
+    connection = create_connection()
+    if connection:
+        try:
+            cursor = connection.cursor()
+            query = 'SELECT * FROM users WHERE username = %s AND password = %s'
+            values = (username, password)
+            cursor.execute(query, values)
+            result = cursor.fetchone()
+
+            if result:
+                print('Login successful')
+            else:
+                print('Invalid credentials')
+        except Error as e:
+            print(f'Error while logging in: {e}')
+        finally:
+            cursor.close()
+            connection.close()
+
 # Usage example
-username = input('Enter username: ')
-password = input('Enter password: ')
-register_user(username, password)
+choice = input('Enter 1 to register, 2 to login: ')
+if choice == '1':
+    username = input('Enter username: ')
+    password = input('Enter password: ')
+    register_user(username, password)
+elif choice == '2':
+    username = input('Enter username: ')
+    password = input('Enter password: ')
+    login_user(username, password)
+else:
+    print('Invalid choice')
