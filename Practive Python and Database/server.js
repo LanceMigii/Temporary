4533ccc2131bdeb917811fe62706767031be81db
LanceMigii <133740971+LanceMigii@users.noreply.github.com>
2023-05-22 12:49:27 +0800

    server.js
    Updated

--- Practive Python and Database/server.js
+++ Practive Python and Database/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
+const session = require('express-session');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,12 +14,35 @@ const connection = mysql.createConnection({
   password: 'jmc12345'
 });
 
+app.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+// Middleware to check if the user is logged in
+const requireLogin = (req, res, next) => {
+  if (req.session && req.session.isLoggedIn) {
+    // User is logged in, proceed to the next middleware
+    next();
+  } else {
+    // User is not logged in, redirect to the login page
+    res.redirect('/home');
+  }
+};
+
 app.get('/home', (req, res) => {
   res.sendFile(__dirname + '/login.html');
 });
 
 app.get('/registration', (req, res) => {
   res.sendFile(__dirname + '/registration.html');
+});
+
+app.get('/api.html', requireLogin, (req, res) => {
+  res.sendFile(__dirname + '/api.html');
 });
 
 app.post('/login', (req, res) => {
@@ -32,10 +56,42 @@ app.post('/login', (req, res) => {
     } else {
       if (results.length > 0) {
         console.log('Login successful');
-        res.status(200).send('Login successful');
+        req.session.isLoggedIn = true; // Set isLoggedIn flag in session
+        res.redirect('/api.html'); // Redirect to api.html upon successful login
       } else {
         console.log('Invalid credentials');
         res.status(401).send('Invalid credentials');
+      }
+    }
+  });
+});
+
+app.post('/registration', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if the username is already taken
+  connection.query('SELECT * FROM users WHERE username = ?', [username], (error, results) => {
+    if (error) {
+      console.error('Error checking username availability:', error);
+      res.status(500).send('Error checking username availability');
+    } else {
+      if (results.length > 0) {
+        // Username is already taken
+        console.log('Username already exists');
+        res.status(409).send('Username already exists');
+      } else {
+        // Username is available, proceed with registration
+        connection.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (error, results) => {
+          if (error) {
+            console.error('Error registering user:', error);
+            res.status(500).send('Error registering user');
+          } else {
+            console.log('Registration successful');
+            req.session.isLoggedIn = true; // Set isLoggedIn flag in session
+            res.redirect('/api.html'); // Redirect to api.html after registration
+          }
+        });
       }
     }
   });
